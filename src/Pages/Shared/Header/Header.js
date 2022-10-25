fd23c24a9ba3caf966674b175c203e60ae796e6a
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-26 01:12:21 +0600

    firebase, react router dom and react bootstrap setup

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,14 +4,33 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 
 
 const Header = () => {
   return (
     <div>
-      
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">skill.Nation</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mx-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link><Link to='/'>Home</Link></Nav.Link>
+              <Nav.Link><Link to='/courses'>Courses</Link></Nav.Link>
+              <Nav.Link><Link to='/faq'>FAQ</Link></Nav.Link>
+              <Nav.Link><Link to='/blog'>Blog</Link></Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Button variant="outline-success">Login</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
