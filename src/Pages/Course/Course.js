fc52b804c3191342fb24163501a9ba91e2ff22e6
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-26 04:45:20 +0600

    Course details page added

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Course = () => {
 
   const course = useLoaderData();
   console.log(course);
 
+  const {details, title, image_url, price, rating, teacher} = course;
+
   return (
-    <div>
-      <h2>{course.title}</h2>
+    <div className='container w-50'>
+      <h2>{title}</h2>
+      <Card className="text-center">
+        <Card.Header>Featured</Card.Header>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            {details}
+          </Card.Text>
+          <Button variant="primary">get premium access</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
     </div>
   );
 };
--- src/Pages/CourseCard/CourseCard.js
+++ src/Pages/CourseCard/CourseCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 const CourseCard = ({category}) => {
 
@@ -13,7 +14,7 @@ const CourseCard = ({category}) => {
           <Card.Img variant="top" src={image_url} />
           <Card.Body>
             <Card.Title>{name}</Card.Title>
-            <Button variant="primary">Check details</Button>
+            <Link to={`/courses/${category.id}`}><Button>Check details</Button></Link>
           </Card.Body>
         </Card>
       </Col>
