1aadc83ab4879d278925192844662e0927212a58
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-26 01:09:58 +0600

    react app is setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import heroImage from '../../assets/hero.png';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <section className="section py-5 my-5">
+        <div className="container">
+          <div className="row justify-content-between align-items-center">
+            <div className="col-lg-7 text-lg-start text-center">
+              <h1 className="mb-4">A broad selection of courses. <br /> Expand your career opportunities </h1>
+              <p className="mb-4">Choose from 204,000 online video courses with new additions published every month.</p>
+              <Button><Link className='text-white' to='/courses'>See our courses</Link></Button>
+            </div>
+            <div className="col-lg-4 d-lg-block d-none">
+              <Image src={heroImage} alt="illustration" className="img-fluid"/>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7GOB5ORP0EAC-9YoP36Rb7MhzhOeKdVE",
+  authDomain: "skill-nation-b6ffb.firebaseapp.com",
+  projectId: "skill-nation-b6ffb",
+  storageBucket: "skill-nation-b6ffb.appspot.com",
+  messagingSenderId: "717496735255",
+  appId: "1:717496735255:web:c212b2c611ea6707ca6e52"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
