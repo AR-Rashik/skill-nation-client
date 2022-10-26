920b8d975561029c32d77073b78799586d7bdd1c
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-10-27 02:27:19 +0600

    React to pdf added

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ReactDOM from "react-dom";
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 const Course = () => {
 
@@ -12,19 +16,24 @@ const Course = () => {
 
   return (
     <div className='container w-50'>
-      <h2>{title}</h2>
-      <Card className="text-center">
-        <Card.Header>Featured</Card.Header>
-        <Card.Img variant="top" src={image_url} />
-        <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            {details}
-          </Card.Text>
-          <Link to={`/checkout/${category_id}`}><Button variant="primary">get premium access</Button></Link>
-        </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
-      </Card>
+      <Pdf targetRef={ref} filename="code-example.pdf">
+          {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
+        </Pdf>
+      <div ref={ref}>
+        <h2>{title}</h2>
+          <Card className="text-center">
+            <Card.Header>Featured</Card.Header>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                {details}
+              </Card.Text>
+              <Link to={`/checkout/${category_id}`}><Button variant="primary">get premium access</Button></Link>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+          </Card>
+      </div>
     </div>
   );
 };
